af37d02eb3cff8dfb9619e84c63d27541a3149c7
piuverse <125109787+upiverse@users.noreply.github.com>
2023-03-12 17:08:21 +0900

    Delete existing code

--- cmpl/lexer/shell.py
+++
@@ -1,9 +0,0 @@
-#shell.py
-import lexer
-
-while True:
-    text = input('lexer >')
-    result, error = lexer.run('<stdin>', text)
-
-    if error: print(error.as_string())
-    else: print(result)
